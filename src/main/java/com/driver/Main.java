60975462213497b00205be9d951f300de5da504c
gundasriharsha <harshagunda2002@gmail.com>
2023-05-12 12:28:35 +0530

    first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,21 @@
 package com.driver;
 
 public class Main {
-
+    public static void main(String[] args) {
+        Product p=new Product();
+        p.product(1,2);
+        p.product(3,4,5);
+        p.product(6.7,8.9);
+    }
+    public static class Product{
+        public int product(int x, int y){
+            return x*y;
+        }
+        public int product(int x, int y, int z){
+            return x*y*z;
+        }
+        public double product(double x, double y){
+            return x*y;
+        }
+    }
 }
